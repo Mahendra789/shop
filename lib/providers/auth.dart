35b4fcd595c4eba93728fe863e1025ab2e9c939f
Mahendra789 <ahada.mahendra@gmail.com>
2020-07-05 11:59:22 +0530

    signup using firebase

---
+++ lib/providers/auth.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signup(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAxlaBJbfM1pDRa2TjPPK4qv6wJNnti16o';
+
+    final response = await http.post(url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }));
+
+    print(json.encode(response.body));
+  }
+}
